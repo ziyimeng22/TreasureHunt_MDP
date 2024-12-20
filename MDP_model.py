ecfcb55e66dcdc588d7e565f862b222c8438157c
Ziyi Meng <ziyi.meng22@gmail.com>
2024-12-20 01:04:32 -0500

    ValueIteration+Simulation

--- MDP_model.py
+++ MDP_model.py
@@ -16,6 +16,7 @@ class BellmanUpdate(object):
         Qs={a:sum([self.transitionFunction(s, a, sPrime)*(self.rewardFunction(s, a, sPrime)+self.gamma*V[sPrime]) for sPrime in self.stateSpace])\
             for a in self.actionSpaceFunction(s)}
         Vs=max(Qs.values())
+        print(Vs)
         return Vs   # maximum expected value (over all actions) for a given state s
 
 # class ValueIteration
@@ -34,6 +35,7 @@ class ValueIteration(object):
                 v = V[s]
                 V[s] = self.bellmanUpdate(s, V)
                 delta = max(delta, abs(v - V[s]))
+        print(V)
         return V   # optimal value function for all states
 
 # class GetPolicy
@@ -53,77 +55,108 @@ class GetPolicy(object):
             for a in self.actionSpaceFunction(s)}        
         optimalActionList = [a for a in self.actionSpaceFunction(s) if abs(Qs[a]-max(Qs.values())) < self.roundingTolerance]
         policy={a: 1/(len(optimalActionList)) for a in optimalActionList}
+        print(policy)
         return policy   # optimal action policy for a given state s
 
 
 # run simulation
 
-stateSpace = [
-    (0,0), (0,1), (0,2),
-    (1,0), (1,1), (1,2),
-    (2,0), (2,1), (2,2)
-]
-goal = (0,2)
-trap = (1,2)
-block = [(1,1)]
-start = random.choice(stateSpace)
-actions = ["up", "down", "left", "right"]
+class Environment(object):
 
-def actionSpaceFunction(s):
-    actions = {
-        "up": (-1, 0),
-        "down": (1, 0),
-        "left": (0, -1),
-        "right": (0, 1)
-    }
-    return actions.values()
+    def __init__(self, stateSpace, goal, trap, block):
+        self.stateSpace=stateSpace
+        self.goal=goal
+        self.trap=trap
+        self.block=block
 
-def transitionFunction(s, a, sPrime):
-    x, y = s
-    xPrime, yPrime = a
-    sNext = (x+xPrime, y+yPrime)
-    if sNext in block or sNext not in stateSpace:
-        return 1 * (s==sPrime)
-    else:
-        return 1 * (sNext==sPrime)
+    def actionSpaceFunction(self, s):
+        actions = {
+            "up": (-1, 0),
+            "down": (1, 0),
+            "left": (0, -1),
+            "right": (0, 1)
+        }
+        return actions.values()
 
-def rewardFunction(s, a, sPrime):
-    if sPrime == goal:
-        return 100
-    elif sPrime == trap:
-        return -100
-    elif sPrime == block:
-        return 0
-    else:
-        return -1
+    def transitionFunction(self, s, a, sPrime):
+        x, y = s
+        xPrime, yPrime = a
+        sNext = (x+xPrime, y+yPrime)
+        if sNext in block or sNext not in self.stateSpace:
+            return 1 * s==sPrime
+        else:
+            return 1 * sNext==sPrime
+
+    def rewardFunction(self, s, a, sPrime):
+        if sPrime == self.goal:
+            return 5
+        elif sPrime == self.trap:
+            return -10
+        elif sPrime == self.block:
+            return 0
+        else:
+            return -1
+        
+class Agent(object):
+    def __init__(self, start):
+        self.state=start
+
+    def updateState(self, s, a):
+        x, y = s
+        dx, dy = a
+        sPrime = (x+dx, y+dy) 
+        return sPrime
+
+class Simulation(object):
     
-gamma = 0.9
-theta = 1e-4
-roundingTolerance = 1e-4
-V = {s:0 for s in stateSpace}
+    def __init__(self, environment, agent, getPolicy):
+        self.environment=environment
+        self.agent=agent
+        self.getPolicy=getPolicy
 
-bellmanUpdate = BellmanUpdate(stateSpace, actionSpaceFunction, transitionFunction, rewardFunction, gamma)
-valueIteration = ValueIteration(stateSpace, theta, bellmanUpdate)
-V = valueIteration(V)
-getPolicy = GetPolicy(stateSpace, actionSpaceFunction, transitionFunction, rewardFunction, gamma, V, roundingTolerance)
-    
-def playGame():
-    state = start
-    print("Starting Grid-World MDP Game!")
-    time.sleep(1)
-    while state != goal and state not in trap:
-        print(f"\nAgent at: {state}")
-        policy = getPolicy(state)
-        actions, probabilities = zip(*policy.items())
-        action = random.choices(actions, probabilities)[0]
-        x, y = state
-        dx, dy = action
-        next_state = (x+dx, y+dy)
-        state = next_state
+    def playGame(self, start):
+        state = start
+        print("Starting Grid-World MDP Game!")
         time.sleep(1)
-    if state == goal:
-        print("\nAgent reached the GOAL! 🎉")
-    elif state in trap:
-        print("\nAgent fell into a TRAP! 😭")       
+        while state != goal:
+            print(f"\nAgent at: {state}")
+            policy = self.getPolicy(state)
+            actions, probabilities = zip(*policy.items())
+            action = random.choices(actions, probabilities)[0]
+            nextState = self.agent.updateState(state, action)
+            if state == trap: 
+                print("\nAgent fell into a TRAP! 😭")
+            state = nextState
+            time.sleep(1)
+        if state == goal:
+            print(f"\nAgent at: {state}")   
+            print("\nAgent reached the GOAL! 🎉")
 
-playGame()
+
+if __name__ == "__main__":
+
+    stateSpace = [
+        (0,0), (0,1), (0,2),
+        (1,0), (1,1), (1,2),
+        (2,0), (2,1), (2,2)
+    ]
+    goal = (0,2)
+    trap = (1,2)
+    block = [(1,1), (0,1)]
+    start = (0,0)
+
+    environment = Environment(stateSpace, goal, trap, block)
+    agent = Agent(start)
+
+    gamma = 0.9
+    theta = 1e-4
+    roundingTolerance = 1e-4
+    V = {s:0 for s in stateSpace}
+
+    bellmanUpdate = BellmanUpdate(stateSpace, environment.actionSpaceFunction, environment.transitionFunction, environment.rewardFunction, gamma)
+    valueIteration = ValueIteration(stateSpace, theta, bellmanUpdate)
+    V = valueIteration(V)
+    getPolicy = GetPolicy(stateSpace, environment.actionSpaceFunction, environment.transitionFunction, environment.rewardFunction, gamma, V, roundingTolerance)
+
+    simulate = Simulation(environment, agent, getPolicy)
+    simulate.playGame(start)
